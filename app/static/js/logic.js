d3a55fe113a32ca6974d704c2271cbdd6cce75fb
Sorted-Filtered <darkmage1972@gmail.com>
2025-01-30 21:17:19 -0600

    Functional Summary of connectors.

--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -36,6 +36,8 @@ function createMap(state) {
 
     console.log(response);
 
+    updateSummary(response);
+
     let marker_limit = response.length;
     let heatArray = [];
   
@@ -54,13 +56,107 @@ function createMap(state) {
     myMap.fitBounds(mapBounds);
     layers.mapMarkers.addTo(myMap);
     console.log(heatArray);
+
     // Use heatArray to create heatmap layer
     L.heatLayer(heatArray, {
       radius: 15,
       blur: 25,
       maxZoom: 10
-    }).addTo(layers.heatLayer)
+    }).addTo(layers.heatLayer);
+
+    // Plot the data
+    plotData(response);
   });
+}
+
+function plotData(response) {
+
+  let sortedData = response.sort((a, b) => b['Open Date'] - a['Open Date']);
+
+  dates = [];
+  sums = [];
+  activeDate = 'none';
+  activeSum = 0;
+
+  let featureLength = sortedData.length;
+
+  // Loop through the features
+  for (let i = 0; i < featureLength; i++) {
+    let feature = sortedData[i];
+
+    if (i == (featureLength - 1)) {
+      dates.push(activeDate);
+      sums.push(activeSum);
+    } else if (i == 0) {
+      activeDate = feature['Open Date'];
+      activeSum = 1;
+    } else if (feature['Open Date'] != activeDate) {
+      dates.push(activeDate);
+      sums.push(activeSum);
+      activeDate = feature['Open Date'];
+      activeSum = 1;
+    } else {
+      activeSum = activeSum + 1;
+    }
+  }
+
+  // Trace1
+  let trace1 = {
+    x: dates,
+    y: sums,
+    type: "bar"
+  };
+
+  // Data array
+  let data = [trace1];
+
+  // Apply a title to the layout
+  let layout = {
+    title: "Electric Charger Openings by Date"
+  };
+
+  // Render the plot to the div tag with id "plot"
+  Plotly.newPlot("plot", data, layout);
+}
+
+// Fill summary element with metadata
+function updateSummary(response) {
+    console.log("Starting function", response);
+    let connectorSummary = {
+      teslaCon: 0,
+      j1772Con: 0,
+      chademoCon: 0,
+      j1772comboCon: 0
+      }
+    for (let i = 0; i < response.length; i++) {
+      let connectors = response[i]["EV Connector Types"];
+      console.log(connectors);
+      if (connectors) {
+        if (connectors.includes("TESLA")) {
+          connectorSummary.teslaCon += 1
+        }
+        if (connectors.includes("J1772")) {
+          connectorSummary.j1772Con += 1
+        }
+        if (connectors.includes("CHADEMO")) {
+          connectorSummary.chademoCon += 1
+        }
+        if (connectors.includes("J1772COMBO")) {
+          connectorSummary.j1772comboCon += 1
+        }
+      else {
+        console.log(connectorSummary)
+      }}
+    }
+    d3.select("#total-tesla").text("Total Tesla: " + connectorSummary.teslaCon);
+    d3.select("#total-J1772").text("Total J1772: " + connectorSummary.j1772Con);
+    d3.select("#total-CHADEMO").text("Total CHADEMO: " + connectorSummary.chademoCon);
+    d3.select("#total-J1772COMBO").text("Total J1772COMBO: " + connectorSummary.j1772comboCon);
+    pieChartSummary(connectorSummary)
+}
+
+function pieChartSummary(connectorSummary) {
+  console.log(connectorSummary);
 }
 
 // Preload the map data with MN data
